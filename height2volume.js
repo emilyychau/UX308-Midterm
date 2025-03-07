4a0b500dcbd7d4e3535498d0bdaca1b5a29f3f35
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-07 00:45:12 +0000

    populate q3

--- height2volume.js
+++ height2volume.js
@@ -1,7 +1,16 @@
 //q3 converts the height of a cube  in meters to the volume of the cube in cubic meters
 
 function height2vol(height){
-    let posHeight = Math.abs(height);
+    if height <= 0 {
+        console.log("The height must be a positive value.");
+    }  
+    else if height < 0 {
+        console.log("The height must be a positive value. The calculation will continue with the absolute value.");
+        let posHeight = Math.abs(height);
+    }
+    else {
+        console.log("Please try again.");
+    }
     let volume = posHeight ** 3;
     return volume;
 }

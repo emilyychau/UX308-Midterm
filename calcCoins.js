6904a54db00312a29a5694c1068503754d5683df
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-07 00:30:28 +0000

    populate q1

--- calcCoins.js
+++ calcCoins.js
@@ -1,6 +1,8 @@
-function calcCoins(nickel, dime, quarter, loonie, toonie){
-    let 
+//q1 Calculate the total value of a set of coins in dollars
 
+function calcCoins(nickel, dime, quarter, loonie, toonie){
+    let total = (nickel * 0.05) + (dime * 0.10) + (quarter * 0.25) + (loonie * 1.00) + (toonie * 2.00);
+    return total;
 }
 
-export {calCoins}
+export {calcCoins}

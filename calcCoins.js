87093788a07e39afcbe58e2dc1702e1a862764e2
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-07 00:26:51 +0000

    populate q2

---
+++ calcCoins.js
@@ -0,0 +1,6 @@
+function calcCoins(nickel, dime, quarter, loonie, toonie){
+    let 
+
+}
+
+export {calCoins}
---
+++ convertFtoC.js
@@ -0,0 +1,8 @@
+//q2 
+
+function fahrenheit2celsius (fahrenheit){
+    let celsius = (fahrenheit - 32) * (5/9);
+    return celsius;
+}
+
+export {fahrenheit2celsius}

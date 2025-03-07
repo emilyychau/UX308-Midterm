81b7f57a39913939f85ad092ebe13a09e27d7a7f
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-07 02:20:41 +0000

    test spec q4

--- spec/membership_spec.js
+++ spec/membership_spec.js
@@ -4,19 +4,25 @@ describe("testing friend discounts", function(){
     it("tests discount with 1 friend", function(){
         let iFriends = 1; 
         let fCost = 100;
-        let fTotal = gymDiscount(iFriends, fCost);
-        expect(fTotal).toBe("47.50");
+        let fTotal = gymDiscount(fCost, iFriends);
+        expect(fTotal.toFixed(2)).toBe("95.00");
     });
     it("tests discount with 2 friends", function(){
         let iFriends = 2; 
         let fCost = 100;
-        let fTotal = gymDiscount(iFriends, fCost);
-        expect(fTotal).toBe("90.00");
+        let fTotal = gymDiscount(fCost, iFriends);
+        expect(fTotal.toFixed(2)).toBe("90.00");
+    });
+    it("tests discount with 3 friends", function(){
+        let iFriends = 3; 
+        let fCost = 100;
+        let fTotal = gymDiscount(fCost, iFriends);
+        expect(fTotal.toFixed(2)).toBe("85.00");
     });
     it("tests discount with 4 friends", function(){
         let iFriends = 4; 
         let fCost = 100;
-        let fTotal = gymDiscount(iFriends, fCost);
-        expect(fTotal).toBe("63.75");
+        let fTotal = gymDiscount(fCost, iFriends);
+        expect(fTotal.toFixed(2)).toBe("85.00");
     });
 })

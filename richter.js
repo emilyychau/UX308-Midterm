c542b4eff95225859e0ad98cdbe1bbc01d88d650
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-07 02:44:59 +0000

    fix the q5 richter scale

--- richter.js
+++ richter.js
@@ -8,16 +8,16 @@ function richter2damagelvl (fIntensity){
     else if (fIntensity >= 0 && fIntensity <= 5){
         sResult = "Little or no damage";
     }
-    else if (fIntensity >= 5.01 && fIntensity <= 5.55){
+    else if (fIntensity >= 5.01 && fIntensity <= 5.5){
         sResult = "Some damage";
     }
-    else if (fIntensity >= 5.56 && fIntensity <= 6.5){
+    else if (fIntensity >= 5.51 && fIntensity <= 6.5){
         sResult = "Serious damage: walls may crack or fall";
     }
-    else if (fIntensity >= 6.51 && fIntensity <= 7.5){
+    else if (fIntensity >= 6.51 && fIntensity <= 7.49){
         sResult = "Disaster: buildings may collapse";
     }
-    else if (fIntensity >= 7.51){
+    else if (fIntensity >= 7.5){
         sResult = "Catastrophe: most buildings destroyed";
     }
 return sResult;

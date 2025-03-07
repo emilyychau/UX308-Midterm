e562bbb1706d200bdd68a4333f3a0849d98ce36c
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-07 02:47:31 +0000

    fix Richter scale again

--- richter.js
+++ richter.js
@@ -5,16 +5,16 @@ function richter2damagelvl (fIntensity){
     if (fIntensity <= -1){
         sResult = "Possible error reading, no Richter Scale intensity detected.";
     }
-    else if (fIntensity >= 0 && fIntensity <= 5){
+    else if (fIntensity >= 0 && fIntensity < 5){
         sResult = "Little or no damage";
     }
-    else if (fIntensity >= 5.01 && fIntensity <= 5.5){
+    else if (fIntensity >= 5 && fIntensity < 5.5){
         sResult = "Some damage";
     }
-    else if (fIntensity >= 5.51 && fIntensity <= 6.5){
+    else if (fIntensity >= 5.5 && fIntensity < 6.5){
         sResult = "Serious damage: walls may crack or fall";
     }
-    else if (fIntensity >= 6.51 && fIntensity <= 7.49){
+    else if (fIntensity >= 6.5 && fIntensity < 7.5){
         sResult = "Disaster: buildings may collapse";
     }
     else if (fIntensity >= 7.5){

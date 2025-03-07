944032fded2e1bf74d6a9ffb916a072ac04e7530
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-07 02:27:03 +0000

    add q4 error msg

--- spec/membership_spec.js
+++ spec/membership_spec.js
@@ -1,6 +1,18 @@
 import { gymDiscount } from "../membership.js";
 
 describe("testing friend discounts", function(){
+    it("tests discount with no friends", function(){
+        let iFriends = 1; 
+        let fCost = 0;
+        let fTotal = gymDiscount(fCost, iFriends);
+        expect(fTotal).toBe("The cost of the initial gym membership cannot be $0 or less");
+    });
+    it("tests discount with no friends", function(){
+        let iFriends = 0; 
+        let fCost = 50;
+        let fTotal = gymDiscount(fCost, iFriends);
+        expect(fTotal.toFixed(2)).toBe("50.00");
+    });
     it("tests discount with 1 friend", function(){
         let iFriends = 1; 
         let fCost = 100;

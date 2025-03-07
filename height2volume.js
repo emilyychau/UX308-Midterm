49ae2d9e7dff3a55f99903caddd07273079a4722
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-07 01:39:19 +0000

    test spec q3

--- height2volume.js
+++ height2volume.js
@@ -1,17 +1,7 @@
 //q3 converts the height of a cube  in meters to the volume of the cube in cubic meters
 
-function height2vol(height){
-    if (height <= 0) {
-        console.log("The height must be a positive value.");
-    }  
-    else if (height < 0) {
-        console.log("The height must be a positive value. The calculation will continue with the absolute value.");
-        posHeight = Math.abs(height);
-    }
-    else {
-        console.log("Please try again.");
-    }
-    let volume = posHeight ** 3;
+function height2vol(height, posHeight){
+    let volume = height ** 3;
     return volume;
 }
 
---
+++ spec/cubeVol_spec.js
@@ -0,0 +1,25 @@
+import {height2vol} from "../height2volume.js";
+
+describe("testing height2volume.js", function(){
+    it ("tests a height a 5m", function(){
+        let height = 5;
+        let volume = height2vol(height);
+        expect(volume.toFixed(2)).toBe("125.00");
+    });
+    it ("tests a height a 1m", function(){
+        let height = 1;
+        let volume = height2vol(height);
+        expect(volume.toFixed(2)).toBe("1.00");
+    });
+    it ("tests a height a 7m", function(){
+        let height = 7;
+        let volume = height2vol(height);
+        expect(volume.toFixed(2)).toBe("343.00");
+    });
+    it ("tests a height a 0m", function(){
+        let height = 0;
+        let volume = height2vol(height);
+        expect(volume.toFixed(2)).toBe("0.00");
+    });
+
+})
